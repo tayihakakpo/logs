b5a1b8e427700608ee2a372b8474551c007d1a36
tayihakakpo <thomas.ayihakakpo@gmail.com>
2018-01-29 19:56:02 +0300

    simple class to manage simple log systems

--- logs.py
+++ logs.py
@@ -14,10 +14,42 @@ __all__ = [
     ]
 
 
+FORMAT = '[%(asctime)s] [%(levelname)s] [%(name)s] [%(funcName)s] %(message)s'
+DATEFORMAT = '%Y-%m-%d %H:%M:%S %z'
+DEFAULT_LEVEL = logging.DEBUG
+
+
 class Log(object):
     """Represents a log system
     with a logger and potentially multiple handlers
     """
-    pass
+
+    def __init__(self, name, level=None, fmt=None, datefmt=None):
+        """Create a logger with a default handler to stream to stdout"""
+
+        logger = logging.getLogger(name)
+        fmt = fmt or FORMAT
+        datefmt = datefmt or DATEFORMAT
+        formatter = logging.Formatter(fmt=fmt, datefmt=datefmt)
+        level = Log.level(level)
+        logger.setLevel(level)
+        stream_handler = logging.StreamHandler()
+        stream_handler.setLevel(level)
+        stream_handler.setFormatter(formatter)
+        logger.addHandler(stream_handler)
+
+        self.logger = logger
+
+    @staticmethod
+    def level(name=None):
+        """Return a logging.name object where "name" is the log level"""
+        name = name or 'DEBUG'
+        try:
+            level = getattr(logging, name)
+        except AttributeError:
+            raise ValueError('Niveau de log inconnu: "{}"'.format(name))
+        else:
+            return level
+
 
 # EOF
